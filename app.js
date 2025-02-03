e069a8b63bb6fa59674981c633e01ede934f184d
PedroXavier90 <p.wandir@yahoo.com.br>
2025-02-03 16:56:18 -0300

    Criando o campo para adicionar amigos.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeDisponivel = [];
+let nomeSorteado = [];
+
+function exibirTextoNaTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1','Jogo do Amigo Secreto');
+}   
+
+exibirMensagemInicial();
+
+
+function adicionarAmigo() {
+    let campoNome = document.querySelector('input');
+    let nome = campoNome.value.trim(); // Remove espaços extras
+
+    if (nome === '') {
+        alert('Digite um nome válido!');
+        return;
+    }
+
+    if (nomeDisponivel.includes(nome)) { // Evita nomes duplicados
+        alert('Este nome já foi adicionado!');
+        return;
+    }
+
+    nomeDisponivel.push(nome);
+    campoNome.value = ''; // Limpa o campo de entrada
+    exibirListaNaTela();
+}
